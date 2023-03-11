3e3bdab762512c4e650f0894537820afa72fa31e
victor Ekundayo <victor.ekundayo@ietech.com.ng>
2023-03-11 04:20:42 +0100

    created Navbarand routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router,Navigate, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Navigate>
+          <Route path="/" exact />
+        </Navigate>
+      </Router>
+    </>
   );
 }
 
